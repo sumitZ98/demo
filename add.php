a7216a00b226742bc32b0fec407303ba85db88c7
Sumit Mitra <sm1282713@gmail.com>
2023-06-14 15:44:48 +0530

    added files

--- add.php
+++ add.php
@@ -1,36 +1,37 @@
 <?php 
 
+	$errors = array( 'email'=>'', 'title'=>'', 'ingredients'=>'');
 	if(isset($_POST['submit'])){
 
 		$email = $title = $ingredients = '';
 		
 		// check email
 		if(empty($_POST['email'])){
-			echo 'An email is required <br />';
+			$errors = 'An email is required <br />';
 		} else{
 			$email = $_POST['email'];
 			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-				echo 'Email must be a valid email address';
+				$errors['email'] = 'Email must be a valid email address';
 			}
 		}
 
 		// check title
 		if(empty($_POST['title'])){
-			echo 'A title is required <br />';
+			$errors = 'A title is required <br />';
 		} else{
 			$title = $_POST['title'];
 			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
-				echo 'Title must be letters and spaces only';
+				$errors[title] = 'Title must be letters and spaces only';
 			}
 		}
 
 		// check ingredients
 		if(empty($_POST['ingredients'])){
-			echo 'At least one ingredient is required <br />';
+			$errors = 'At least one ingredient is required <br />';
 		} else{
 			$ingredients = $_POST['ingredients'];
 			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
-				echo 'Ingredients must be a comma separated list';
+				$errors['ingredients'] = 'Ingredients must be a comma separated list';
 			}
 		}
 
@@ -48,10 +49,13 @@
 		<form class="white" action="add.php" method="POST">
 			<label>Your Email</label>
 			<input type="text" name="email">
+			<div class="red-text"><?php echo $errors['email']?></div>
 			<label>Pizza Title</label>
 			<input type="text" name="title">
+			<div class="red-text"><?php echo $errors['title']?></div>
 			<label>Ingredients (comma separated)</label>
 			<input type="text" name="ingredients">
+			<div class="red-text"><?php echo $errors['ingredients']?></div>
 			<div class="center">
 				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
 			</div>
